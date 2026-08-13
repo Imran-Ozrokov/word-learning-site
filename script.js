c6bf859cce9fb2aeb62f2f2513bdbc637a02ff63
Imran-Ozrokov <imran17042012imran@gmail.com>
2026-08-13 19:45:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const words = [
-    { foreign: "Apple", russian: "Яблоко" },
-    { foreign: "Book", russian: "Книга" },
-    { foreign: "Laptop", russian: "Ноутбук" }
-];
-let index = 0;
 const card = document.getElementById("card");
-const button = document.getElementById("next-btn");
+const rusWord = document.getElementById("word-rus");
 
-button.addEventListener("click", () => {
-    index = (index + 1) % words.length;
-    card.innerText = `${words[index].foreign} — ${words[index].russian}`;
+card.addEventListener("click", () => {
+    if (rusWord.style.display === "none") {
+        rusWord.style.display = "block";
+    } else {
+        rusWord.style.display = "none";
+    }
 });
